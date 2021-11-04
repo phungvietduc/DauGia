00e1b93a00c11d882c15cafc8cc386803251430a
phungvietduc <90165458+phungvietduc@users.noreply.github.com>
2021-11-04 23:52:52 +0700

    Add files via upload
    thêm session

--- BTL-daugia/process-login.php
+++ BTL-daugia/process-login.php
@@ -4,13 +4,13 @@ include 'config/config.php';
 if( isset($_POST["submit"]) && $_POST["username"] != '' && $_POST["password"] != ''){
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password' ";
+    $sql = "SELECT * FROM nguoidung WHERE TenDangNhap = '$username' AND MatKhau = '$password' ";
     $user = mysqli_query($conn, $sql);
     if(mysqli_num_rows($user)>0){
-        $_SESSION["user"] = $username;
+        $_SESSION['username'] = $username;
         header("location:hienthi/index.php");
     }else{
-        echo '<script language="javascript">alert("Tài khoản hoặc mật khẩu không chính sác"); window.location="register.php";</script>';
+        echo '<script language="javascript">alert("Tài khoản hoặc mật khẩu không chính xác"); window.location="login.php";</script>';
     }
 }else{
     header("location:login.php");
