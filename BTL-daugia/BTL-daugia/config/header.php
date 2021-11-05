6b0bfa74b4cf830c27907fccafa2caf9f2b7fe8d
NamQuang001 <90582581+NamQuang001@users.noreply.github.com>
2021-11-05 11:15:49 +0700

    full cai lm dc o day nhe

---
+++ BTL-daugia/BTL-daugia/config/header.php
@@ -0,0 +1,18 @@
+<header class="bg-dark text-white p-0" style="height: 27px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" style="margin-top : 0px">
+                <p>Chính sách đổi trả</p>
+            </div>
+            <div class="col-md-3" style="margin-top : 0px">
+                <p> Phí giao hàng: 49.000đ</p>
+            </div>
+            <div class="col-md-3" style="margin-top : 0px">
+                <p>Tất cả sản phẩm từ 1.000đ</p>
+            </div>
+            <div class="col-md-3" style="margin-top : 0px">
+                <p>(024).789.999</p>
+            </div>
+        </div>
+    </div>
+</header>
